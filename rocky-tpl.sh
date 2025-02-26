e27e6b2bfb8f886b0bdf5d04be77fe53dd07e2d9
Naililruojnob <146110607+Naililruojnob@users.noreply.github.com>
2025-02-26 16:26:34 +0100

    Create rocky-tpl.sh

---
+++ rocky-tpl.sh
@@ -0,0 +1,65 @@
+#!/bin/bash
+
+# Variables
+id=9997
+vm_name="rocky-cloudinit-tpl"
+memory=4096
+cores=2
+disk_size="20G"
+debian_image_url="https://dl.rockylinux.org/pub/rocky/9/images/x86_64/Rocky-9-GenericCloud-Base.latest.x86_64.qcow2"
+debian_image_name="Rocky-9-GenericCloud-Base.latest.x86_64.qcow2"
+storage_pool="local-lvm"
+network_bridge="vmbr0"
+scsihw_type="virtio-scsi-pci"
+cloudinit_drive="local-lvm:cloudinit"
+
+echo "Starting the script to create a Proxmox template with ID $id..."
+
+apt update
+apt install libguestfs-tools qemu-utils wget -y
+
+# Download Debian image
+echo "Downloading Debian image..."
+wget $debian_image_url -O $debian_image_name
+echo "Debian image downloaded."
+
+# Customize the image to include qemu-guest-agent
+echo "Customizing the Debian image to include qemu-guest-agent..."
+virt-customize -a $debian_image_name --install qemu-guest-agent
+echo "Debian image has been customized."
+
+# Create the VM
+echo "Creating the VM with ID $id..."
+qm create $id --name $vm_name --net0 virtio,bridge=$network_bridge --scsihw $scsihw_type --pool Template
+echo "VM created."
+
+# Import the disk
+echo "Importing the disk to the VM..."
+qm importdisk $id $debian_image_name $storage_pool --format raw
+qm set $id --scsi0 $storage_pool:vm-$id-disk-0
+echo "Disk imported."
+
+# Resize the disk
+echo "Resizing the disk..."
+qm disk resize $id scsi0 $disk_size
+echo "Disk resized."
+
+# Set VM options
+echo "Setting VM options..."
+qm set $id --boot order=scsi0
+qm set $id --cpu host --cores $cores --memory $memory
+qm set $id --ide2 $cloudinit_drive
+qm set $id --agent enabled=1
+echo "VM options set."
+
+# Convert to template
+echo "Converting VM to template..."
+qm template $id
+echo "VM converted to template."
+
+# Clean up
+echo "Cleaning up the downloaded image..."
+rm $debian_image_name
+echo "Cleanup done."
+
+echo "Script completed. Template with ID $id has been created successfully."
